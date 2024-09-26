869f26608ee073a7a454ec1a98c4e57619981b11
fasjesus <developerflavia@gmail.com>
2024-09-26 20:33:06 -0300

    resolving conflict with student

--- backend/UescColcicAPI/Program.cs
+++ backend/UescColcicAPI/Program.cs
@@ -7,8 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddScoped<IStudentsCRUD, StudentsCRUD>();
-
 builder.Services.AddScoped<ISkillsCRUD, SkillsCRUD>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
