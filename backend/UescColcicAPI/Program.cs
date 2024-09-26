0254d075c0699c5c74755babd334652c2ca95d95
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-09-26 12:54:33 -0300

    Add ProjectsController class and implement CRUD operations

--- backend/UescColcicAPI/Program.cs
+++ backend/UescColcicAPI/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddScoped<IStudentsCRUD, StudentsCRUD>();
 builder.Services.AddScoped<IProfessorsCRUD, ProfessorsCRUD>();
+builder.Services.AddScoped<IProjectsCRUD, ProjectsCRUD>();
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
