36ffb3d93d2d5e867563cec32b85523acfaca337
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-09-26 06:05:33 -0300

    Add ProfessorViewModel class

---
+++ backend/UescColcicAPI.Service/ViewModel/ProfessorViewModel.cs
@@ -0,0 +1,10 @@
+namespace UescColcicAPI.Services.ViewModel;
+
+public class ProfessorViewModel
+{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Department { get; set; }
+    public string? Bio { get; set; }
+
+}
