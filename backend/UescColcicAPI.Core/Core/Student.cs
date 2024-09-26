f1eaed966fde66f638014b404bff04cf12812dfe
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-09-26 13:47:34 -0300

    Add properties to Student class

--- backend/UescColcicAPI.Core/Core/Student.cs
+++ backend/UescColcicAPI.Core/Core/Student.cs
@@ -4,5 +4,10 @@ namespace UescColcicAPI.Core;
 
 public class Student
 {
-   
+   public int StudentId { get; set; }
+   public string? Registration { get; set; }
+   public string? Name { get; set; } 
+   public string? Email { get; set; }
+   public string? Course { get; set; }
+   public string? Bio { get; set; }
 }
