0b41a9637f4a45bc0c7efa4b74d1a0c50bdf1bb5
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-09-26 06:02:11 -0300

    Add Professor class to Core namespace

---
+++ backend/UescColcicAPI.Core/Core/Professor.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace UescColcicAPI.Core
+{
+    public class Professor
+    {
+        public int ProfessorId { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Department { get; set; }
+        public string? Bio { get; set; }
+
+    }
+}
