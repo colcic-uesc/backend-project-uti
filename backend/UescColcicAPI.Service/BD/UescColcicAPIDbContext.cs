13df41b8aeb44337828e437ee5c7bb7f98dcee40
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-10-09 21:40:54 -0300

    Refactor database connection configuration

--- backend/UescColcicAPI.Service/BD/UescColcicAPIDbContext.cs
+++ backend/UescColcicAPI.Service/BD/UescColcicAPIDbContext.cs
@@ -1,13 +1,23 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using UescColcicAPI.Core;
 
 namespace UescColcicAPI.Services.BD
 {
     public class UescColcicAPIDbContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+
+        
+        public UescColcicAPIDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var connectionString = "Server=localhost;Database=colcicdb;User=root;Password=251417@BAV;";
+            
+            var connectionString = _configuration.GetConnectionString("UescColcicDb");
             optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 21)));
         }
 
@@ -16,16 +26,14 @@ namespace UescColcicAPI.Services.BD
         public DbSet<Student> Students { get; set; }
         public DbSet<Skill> Skills { get; set; }
 
-         protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-                modelBuilder.Entity<Professor>().HasKey(x => x.ProfessorId);
-                modelBuilder.Entity<Professor>().HasMany<Project>(x => x.Projects).WithOne(x => x.Professor).HasForeignKey(x => x.ProfessorId);
+            modelBuilder.Entity<Professor>().HasKey(x => x.ProfessorId);
+            modelBuilder.Entity<Professor>().HasMany<Project>(x => x.Projects).WithOne(x => x.Professor).HasForeignKey(x => x.ProfessorId);
 
-
-                modelBuilder.Entity<Project>().HasKey(x => x.ProjectId);
-                modelBuilder.Entity<Student>().HasKey(x => x.StudentId);
-                modelBuilder.Entity<Skill>().HasKey(x => x.SkillId);
-
+            modelBuilder.Entity<Project>().HasKey(x => x.ProjectId);
+            modelBuilder.Entity<Student>().HasKey(x => x.StudentId);
+            modelBuilder.Entity<Skill>().HasKey(x => x.SkillId);
         }
     }
 }
--- backend/UescColcicAPI/Program.cs
+++ backend/UescColcicAPI/Program.cs
@@ -1,23 +1,28 @@
+using Microsoft.EntityFrameworkCore;
 using UescColcicAPI.Services.BD;
 using UescColcicAPI.Services.BD.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<UescColcicAPIDbContext>();
+builder.Services.AddDbContext<UescColcicAPIDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("UescColcicDb");
+    var serverVersion = ServerVersion.AutoDetect(connectionString);
+    options.UseMySql(connectionString, serverVersion);
+});
 
 builder.Services.AddScoped<IStudentsCRUD, StudentsCRUD>();
 builder.Services.AddScoped<IProfessorsCRUD, ProfessorsCRUD>();
 builder.Services.AddScoped<IProjectsCRUD, ProjectsCRUD>();
 builder.Services.AddScoped<ISkillsCRUD, SkillsCRUD>();
+
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
