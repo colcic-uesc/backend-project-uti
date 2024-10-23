24fde30418697516a923ac4cb02fe342655bbd76
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-10-23 01:44:31 -0300

    Add JWT authentication and middleware for logging and response headers

--- backend/UescColcicAPI/Program.cs
+++ backend/UescColcicAPI/Program.cs
@@ -1,7 +1,36 @@
 using UescColcicAPI.Services.BD;
 using UescColcicAPI.Services.BD.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using UescColcicAPI.Middlewares;
+using UescColcicAPI.Services.Auth;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do JWT
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "colcic.uesc.br",
+        ValidAudience = "colcic.uesc.br",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ChaveSecretaColcicComMaisDe16Caracteres"))
+    };
+});
+
+// Adicionar autorização
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
 
 builder.Services.AddDbContext<UescColcicAPIDbContext>();
 
@@ -9,23 +38,35 @@ builder.Services.AddScoped<IStudentsCRUD, StudentsCRUD>();
 builder.Services.AddScoped<IProfessorsCRUD, ProfessorsCRUD>();
 builder.Services.AddScoped<IProjectsCRUD, ProjectsCRUD>();
 builder.Services.AddScoped<ISkillsCRUD, SkillsCRUD>();
-builder.Services.AddControllers();
+builder.Services.AddScoped<AuthService>();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Configuração do Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuração do pipeline de requisições
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Middleware de autenticação JWT
+app.UseAuthentication();
+
+// Middleware para log de eventos
+app.UseMiddleware<EventLogMiddleware>();
+
+// Middleware para adicionar cabeçalhos à resposta
+app.UseMiddleware<ResponseAppendMiddleware>();
+
+// Middleware de autorização (JWT)
 app.UseAuthorization();
 
+// Mapeamento dos controllers
 app.MapControllers();
 
 app.Run();
