aea1459285e4efa01b6cff7641245a6ed3cacafe
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-10-26 10:15:47 -0300

    Add migration for User table

--- backend/UescColcicAPI.Service/Migrations/20241026115841_AddUserTable.Designer.cs
+++ backend/UescColcicAPI.Service/Migrations/20241026115841_AddUserTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UescColcicAPI.Services.BD;
 
@@ -11,9 +12,11 @@ using UescColcicAPI.Services.BD;
 namespace UescColcicAPI.Services.Migrations
 {
     [DbContext(typeof(UescColcicAPIDbContext))]
-    partial class UescColcicAPIDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241026115841_AddUserTable")]
+    partial class AddUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
